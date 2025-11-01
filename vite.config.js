a8a26e145d9587e278178c6b2477172373b6721c
imandevt <abdulmuminyusuf10@gmail.com>
2025-11-01 14:03:09 +0100

    json adjustment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // ⚠️ Replace 'your-repo-name' with the actual name of your GitHub repo
+
 export default defineConfig({
   plugins: [react()],
-  base: '/react-week3/', // 👈 must match your repo name exactly
-})
+  base: "/react-js-jsx-and-css-mastering-front-end-development-imandevt/"
+});
