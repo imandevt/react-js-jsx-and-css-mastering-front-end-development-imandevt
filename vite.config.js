355099723b07e8c0a16a491411ecdd18348545c3
imandevt <abdulmuminyusuf10@gmail.com>
2025-11-01 12:27:20 +0100

    latest changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ⚠️ Replace 'your-repo-name' with the actual name of your GitHub repo
 export default defineConfig({
   plugins: [react()],
+  base: '/react-week3/', // 👈 must match your repo name exactly
 })
